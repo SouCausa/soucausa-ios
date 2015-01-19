414fb86756fad7328f94a4a79c494bd401af60bc
Felipe Saruhashi <felipesaruhashi@MacBook-Pro-de-Felipe.local>
2015-01-18 22:00:57 -0200

    Sou Causa Project Creation
    Creates the empty project and with the .gitignore.

---
+++ Sou Causa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sou Causa
+//
+//  Created by Felipe Saruhashi on 1/18/15.
+//  Copyright (c) 2015 Felipe Saruhashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
