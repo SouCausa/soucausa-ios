a8d6b4f1d9e212ee2a83945314f2274734b9a763
Felipe Saruhashi <felipesaruhashi@MacBook-Pro-de-Felipe.local>
2015-02-03 00:43:28 -0200

    Add swipe gesture to change tabs.

--- Sou Causa/MainTabBarController.swift
+++ Sou Causa/MainTabBarController.swift
@@ -1,19 +1,20 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  Sou Causa
 //
-//  Created by Felipe Saruhashi on 1/18/15.
+//  Created by Felipe Saruhashi on 2/2/15.
 //  Copyright (c) 2015 Felipe Saruhashi. All rights reserved.
 //
 
+import Foundation
+
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UITabBarController {
     
-    required init(coder aDecoder: NSCoder) {
-        super.init()
+    override func viewDidAppear(animated: Bool) {
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,29 +29,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func viewDidAppear(animated: Bool) {
-    }
-    
     func swipedView(gesture: UIGestureRecognizer){
+        
+        let tabCount = self.viewControllers?.count as Int!
         
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
             
             switch swipeGesture.direction {
             case UISwipeGestureRecognizerDirection.Right:
-                println("Swiped right")
+                
+                if self.selectedIndex < (tabCount - 1) {
+                    self.selectedIndex = 1
+                }
+
             case UISwipeGestureRecognizerDirection.Left:
-                println("Swiped left")
+                if self.selectedIndex > 0 {
+                    self.selectedIndex = 0
+                }
             default:
                 break
             }
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
